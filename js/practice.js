58a0de40f08c8121fb44056fdbecf0553e98618c
ボイビ <>
2022-06-15 00:06:36 +0900

    upload

--- js/practice.js
+++ js/practice.js
@@ -1,7 +1,8 @@
 $(":button").click(function() {  
     $("#jqs-1 p span").text($(":text").val());
   });
-  $(function(){
-    $("#jqs-1 p span").load("practice.html");
-  });
+
+  // $(function(){
+  //   $("#jqs-1 p span").load("practice.html　jqs-1 p span");
+  // });
 
