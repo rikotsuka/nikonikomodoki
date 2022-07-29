504e1835a911b6c853024a6af3b602e832272fc4
ボイビ <>
2022-07-30 02:22:50 +0900

    upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,95 @@
-(function($) {
 
-	"use strict";
+//定数
+let par1 = "所属企業：";
+let par2 = "担当プロジェクト：";
+let par3 = "専門領域：";
+let par4 = "強み：";
 
-	$(".toggle-password").click(function() {
+window.onload = function(){
+hyouzi()
+}
 
-  $(this).toggleClass("fa-eye fa-eye-slash");
-  var input = $($(this).attr("toggle"));
-  if (input.attr("type") == "password") {
-    input.attr("type", "text");
-  } else {
-    input.attr("type", "password");
-  }
-});
+function hyouzi(){
+var token1 = localStorage.getItem("data1");
+// nullか確認
+if (token1 == "") {
+document.getElementById("text1").innerHTML=par1;
+}else{
+document.getElementById("text1").innerHTML=token1;
+}
 
-})(jQuery);
+var token2 = localStorage.getItem("data2");
+// nullか確認
+if (token2 == "") {
+document.getElementById("text2").innerHTML=par2;
+}else{
+document.getElementById("text2").innerHTML=token2;
+}
+
+var token3 = localStorage.getItem("data3");
+// nullか確認
+if (token3 == "") {
+document.getElementById("text3").innerHTML=par3;
+}else{
+document.getElementById("text3").innerHTML=token3;
+}
+
+var token4 = localStorage.getItem("data4");
+// nullか確認
+if (token4 == "") {
+document.getElementById("text4").innerHTML=par4;
+}else{
+document.getElementById("text4").innerHTML=token4;
+}
+}
+// コメント1保存
+function save1() {
+var mydata1 = par1+document.getElementById("mydata_in1").value;
+localStorage.setItem("data1", mydata1);
+hyouzi();
+}
+// コメント1削除
+function del1() {
+localStorage.setItem("data1",par1);
+document.getElementById("mydata_in1").value="";
+hyouzi();
+}
+
+// コメント2保存
+function save2() {
+var mydata2 =par2+document.getElementById("mydata_in2").value;
+localStorage.setItem("data2", mydata2);
+hyouzi();
+}
+// コメント2削除
+function del2() {
+localStorage.setItem("data2",par2);
+document.getElementById("mydata_in2").value="";
+hyouzi();
+}
+
+// コメント3保存
+function save3() {
+var mydata3 = par3+document.getElementById("mydata_in3").value;
+localStorage.setItem("data3", mydata3);
+hyouzi();
+}
+// コメント2削除
+function del3() {
+localStorage.setItem("data3",par3);
+document.getElementById("mydata_in3").value="";
+hyouzi();
+}
+
+// コメント4保存
+function save4() {
+var mydata4 = par4+document.getElementById("mydata_in4").value;
+localStorage.setItem("data4", mydata4);
+hyouzi();
+}
+// コメント4削除
+function del4() {
+localStorage.setItem("data4",par4);
+document.getElementById("mydata_in4").value="";
+hyouzi();
+}
